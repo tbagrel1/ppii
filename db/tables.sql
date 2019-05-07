fa70a85a6577d7ba50e21d4cbe5b4f510130170e
ADAM Timothee <timothee.adam@telecomnancy.eu>
2019-05-07 19:56:57 +0200

    Modification de la taille des attributs de type NUMBER

--- db/tables.sql
+++ db/tables.sql
@@ -16,18 +16,18 @@ CREATE TABLE Airport (
 )
 
 CREATE TABLE Path (
-    id NUMBER(16) NOT NULL,
-    real_step_nb NUMBER(4),
-    db_step_nb NUMBER(4) NOT NULL,
+    id NUMBER(8) NOT NULL,
+    real_step_nb NUMBER(8),
+    db_step_nb NUMBER(8) NOT NULL,
     real_distance BINARY_DOUBLE,
     straight_distance BINARY_DOUBLE NOT NULL,
     PRIMARY KEY (id)
 )
 
 CREATE TABLE AirportPath (
-    path_id NUMBER(16) NOT NULL,
+    path_id NUMBER(8) NOT NULL,
     airport_icao CHAR(4) NOT NULL,
-    step_no NUMBER(4) NOT NULL,
+    step_no NUMBER(8) NOT NULL,
     PRIMARY KEY (path_id, airport_icao),
     FOREIGN KEY (path_id) REFERENCES Path(id),
     FOREIGN KEY (airport_icao) REFERENCES Airport(icao)
@@ -55,13 +55,13 @@ CREATE TABLE Plane (
 )
 
 CREATE TABLE Fleet (
-    id NUMBER(16) NOT NULL,
+    id NUMBER(8) NOT NULL,
     plane_nb NUMBER(8),
     PRIMARY KEY (id)
 )
 
 CREATE TABLE PlaneFleet (
-    fleet_id NUMBER(16) NOT NULL,
+    fleet_id NUMBER(8) NOT NULL,
     plane_iata CHAR(3) NOT NULL,
     PRIMARY KEY (fleet_id, plane_iata),
     FOREIGN KEY (fleet_id) REFERENCES Fleet(id),
@@ -70,8 +70,8 @@ CREATE TABLE PlaneFleet (
 
 CREATE TABLE Exploitation,
     airline_icao CHAR(4) NOT NULL,
-    fleet_id NUMBER(16) NOT NULL,
-    path_id NUMBER(16) NOT NULL,
+    fleet_id NUMBER(8) NOT NULL,
+    path_id NUMBER(8) NOT NULL,
     flight_no CHAR(8),
     is_codeshare NUMBER(1),
     PRIMARY KEY (airline_icao, fleet_id, path_id, flight_no),
