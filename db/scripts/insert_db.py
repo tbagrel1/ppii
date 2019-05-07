c5776775fe250846329dedb0df5db7055983e7aa
BAGREL Thomas <thomas.bagrel@telecomnancy.eu>
2019-05-07 18:55:38 +0200

    Fichier vide

---
+++ db/scripts/insert_db.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Fonctions utilitaires pour traiter/nettoyer les bases de données rendues
+disponibles par https://openflights.org/.
+"""
+
+__author__ = "Timothée Adam, Thomas Bagrel"
+__copyright__ = "Copyright 2019, PPII-A1"
+__credits__ = ["Thimothée Adam", "Thomas Bagrel"]
+__license__ = "Private"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
